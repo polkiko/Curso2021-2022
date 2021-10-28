aba8229299e5b0f7056dc261aaf9ef5fafbcaa43
VMusat <valii289@gmail.com>
2021-10-28 13:12:03 +0200

    New Queries for web

--- HandsOn/Group01/applicationWeb/colegiosApp/colegiosApp/querysSparql.py
+++ HandsOn/Group01/applicationWeb/colegiosApp/colegiosApp/querysSparql.py
@@ -1,1 +1,58 @@
 # Querys Sparql
+import rdflib
+from rdflib.plugins.sparql import prepareQuery
+
+
+class Colegios:
+
+    def numColegios(self):
+        g = rdflib.Graph()
+
+        g.parse("../../../rdf/output-with-links.nt")
+
+        q = """
+                PREFIX  xsd: <http://www.w3.org/2001/XMLSchema#>
+                PREFIX  cap: <http://www.colegiosapp.org/ontology#>
+                PREFIX  dbo: <http://dbpedia.org/ontology#>
+                PREFIX  owl: <http://www.w3.org/2002/07/owl#>
+
+                SELECT (count(?centro) as ?c)
+                    WHERE{
+                    ?centro cap:nameSchool ?name.      
+                }
+                """
+        gres = g.query(q)
+        resultado = []
+        for row in gres:
+            print(row[0])
+            resultado.append(row[0])
+        return resultado
+
+    def nombreColegio(self, nombre):
+        g = rdflib.Graph()
+
+        g.parse("../../../rdf/output-with-links.nt")
+
+        q = f"""
+        PREFIX  xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX  cap: <http://www.colegiosapp.org/ontology#>
+        PREFIX  dbo: <http://dbpedia.org/ontology#>
+        PREFIX  owl: <http://www.w3.org/2002/07/owl#>
+
+        SELECT ?centro
+            WHERE{{
+            ?centro cap:nameSchool ?name
+               FILTER regex(?name , "{nombre}").
+        }} LIMIT 50
+        """
+        gres = g.query(q)
+        resultado = []
+        for row in gres:
+            print(row[0])
+            resultado.append(row[0])
+        return resultado
+
+
+aux = Colegios()
+# print(aux.nombreColegio("LOS"))
+print(aux.numColegios())
