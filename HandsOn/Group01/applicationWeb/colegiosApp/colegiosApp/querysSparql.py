697b33a8534cd71c8da0b8f1597bfa69ea2d9d6b
gor2000 <jorge.garciama@alumnos.upm.es>
2021-10-28 21:22:02 +0200

    Update querys

--- HandsOn/Group01/applicationWeb/colegiosApp/colegiosApp/querysSparql.py
+++ HandsOn/Group01/applicationWeb/colegiosApp/colegiosApp/querysSparql.py
@@ -18,7 +18,7 @@ class Colegios:
 
                 SELECT (count(?centro) as ?c)
                     WHERE{
-                    ?centro cap:nameSchool ?name.      
+                    ?centro cap:idSchool ?name.      
                 }
                 """
         gres = g.query(q)
@@ -36,15 +36,25 @@ class Colegios:
         PREFIX  dbo: <http://dbpedia.org/ontology#>
         PREFIX  owl: <http://www.w3.org/2002/07/owl#>
 
-        SELECT ?centro
+        SELECT ?name ?tipoVia ?nomCalle ?numCalle
             WHERE{{
+            ?centro cap:hasAddress ?calle.
+            ?calle cap:hasNameAddress ?nomCalle.
+            ?calle cap:hasType ?tipoVia.
+            ?calle cap:hasNumber ?numCalle.
             ?centro cap:nameSchool ?name
                FILTER regex(?name , "{nombre}").
         }} LIMIT 50
         """
         gres = g.query(q)
-        resultado = []
+        resultado = [[], []]
         for row in gres:
-            print(row[0])
-            resultado.append(row[0])
+            resultado[0].append(row[0])
+            auxCalle = row[1] + " " + row[2] + ", " + row[3]
+            #print(row[0], auxCalle)
+            resultado[1].append(auxCalle)
+
         return resultado
+
+aux = Colegios()
+aux.nombreColegio('SAN BLAS')
